9415a6165e93777d5d06ec91549148db1accda47
Lagooneng <hyunwoo122490@gmail.com>
2024-07-28 19:08:45 +0900

    Add Button Selection UI

--- Assets/01_Scripts/01_Unit/ChangeTarget.cs
+++ Assets/01_Scripts/01_Unit/ChangeTarget.cs
@@ -9,6 +9,15 @@ namespace Agit.FortressCraft
         private NormalUintSpawner[] spawners;
         private NormalUintSpawner targetSpawner;
         private Button[] btns;
+        private Button attackBtn;
+        private Color orgColor;
+        private Color unselectedColor;
+
+        private string buttonNameA = "Button_A";
+        private string buttonNameB = "Button_B";
+        private string buttonNameC = "Button_C";
+        private string buttonNameD = "Button_D";
+        private string buttonNameAttack = "Button_Attack";
 
         private void Awake()
         {
@@ -17,30 +26,32 @@ namespace Agit.FortressCraft
 
             foreach (Button btn in btns)
             {
-                if (btn.transform.name == "Button_A")
+                if (btn.transform.name == buttonNameA )
                 {
-                    btn.onClick.AddListener(setTargetA);
+                    btn.onClick.AddListener(SetTargetA);
                 }
-                else if (btn.transform.name == "Button_B")
+                else if (btn.transform.name == buttonNameB )
                 {
-                    btn.onClick.AddListener(setTargetB);
+                    btn.onClick.AddListener(SetTargetB);
                 }
-                else if (btn.transform.name == "Button_C")
+                else if (btn.transform.name == buttonNameC )
                 {
-                    btn.onClick.AddListener(setTargetC);
+                    btn.onClick.AddListener(SetTargetC);
                 }
-                else if (btn.transform.name == "Button_D")
+                else if (btn.transform.name == buttonNameD )
                 {
-                    btn.onClick.AddListener(setTargetD);
+                    btn.onClick.AddListener(SetTargetD);
                 }
-                else if (btn.transform.name == "Button_Attack")
+                else if (btn.transform.name == buttonNameAttack )
                 {
+                    attackBtn = btn;
                     btn.onClick.AddListener(SetAttack);
                 }
             }
+
+            orgColor = btns[0].GetComponent<Image>().color;
+            unselectedColor = new Color(0.5f, 0.5f, 0.5f);
         }
-
-
 
         private void FixedUpdate()
         {
@@ -59,40 +70,90 @@ namespace Agit.FortressCraft
                     break;
                 }
             }
+
+            switch( targetSpawner.Target )
+            {
+                case "A":
+                    UpdateTargetButtonColor(buttonNameA);
+                    break;
+                case "B":
+                    UpdateTargetButtonColor(buttonNameB);
+                    break;
+                case "C":
+                    UpdateTargetButtonColor(buttonNameC);
+                    break;
+                case "D":
+                    UpdateTargetButtonColor(buttonNameD);
+                    break;
+            }
         }
 
-        public void setTargetA()
+        // 골라진 타겟만 색을 바꾸도록 설정 
+        private void UpdateTargetButtonColor(string name)
+        {
+            foreach( Button btn in btns )
+            {
+                if (btn.transform.name == buttonNameAttack) continue;
+
+                if( btn.transform.name == name )
+                {
+                    btn.GetComponent<Image>().color = orgColor;
+                }
+                else
+                {
+                    btn.GetComponent<Image>().color = unselectedColor;
+                }
+            }
+        }
+
+        private void UpdateAttackButtonColor()
+        {
+            if(targetSpawner.AttackEnabled)
+            {
+                attackBtn.GetComponent<Image>().color = orgColor;
+            }
+            else
+            {
+                attackBtn.GetComponent<Image>().color = unselectedColor;
+            }
+        }
+
+        public void SetTargetA()
         {
             if (targetSpawner == null) return;
             Debug.Log("Set as A");
             targetSpawner.RPCTargetChange("A");
+            UpdateTargetButtonColor(buttonNameA);
         }
 
-
-        public void setTargetB()
+        public void SetTargetB()
         {
             if (targetSpawner == null) return;
             Debug.Log("Set as B");
             targetSpawner.RPCTargetChange("B");
+            UpdateTargetButtonColor(buttonNameB);
         }
 
-        public void setTargetC()
+        public void SetTargetC()
         {
             if (targetSpawner == null) return;
             Debug.Log("Set as C");
             targetSpawner.RPCTargetChange("C");
+            UpdateTargetButtonColor(buttonNameC);
         }
 
-        public void setTargetD()
+        public void SetTargetD()
         {
             if (targetSpawner == null) return;
             Debug.Log("Set as D");
             targetSpawner.RPCTargetChange("D");
+            UpdateTargetButtonColor(buttonNameD);
         }
 
         public void SetAttack()
         {
             targetSpawner.RPCChangeAttackEnabled();
+            UpdateAttackButtonColor();
         }
 
     }
